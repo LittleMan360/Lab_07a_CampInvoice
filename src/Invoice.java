ec7a5be469c12201f8c12a821fd94b4fd169abe5
dalto <dalton.m.towe@gmail.com>
2024-03-11 00:55:16 -0400

    invoice created

--- src/Invoice.java
+++ src/Invoice.java
@@ -1,2 +1,43 @@
-public class Invoice {
+import java.util.ArrayList;
+
+public class Invoice
+{
+    private Address billingAddress;
+    private ArrayList<LineItem> items;
+
+    public Invoice(Address address)
+    {
+        billingAddress = address;
+        items = new ArrayList<LineItem>();
+    }
+
+    public void addProduct(Product product, int quantity)
+    {
+        LineItem item = new LineItem(product, quantity);
+        items.add(item);
+    }
+
+    public String formatInvoice()
+    {
+        String r = "                       I N V O I C E\n\n" + billingAddress.AddressFormat() + "\n";
+        for(int i = 0; i < 58; i++){r = r+"=";}
+        r = r + String.format("\n\n%-30s%8s%7s%9s\n", "Description", "Price", "Qty", "Total");
+        for(int i = 0; i < 58; i++){r = r+"-";}
+        r = r+"\n";
+        for(LineItem itemLI : items){r = r + itemLI.productLineFormat() + "\n";}
+        for(int i = 0; i < 58; i++){r = r+"=";}
+        r = r+"\n";
+        r = r + String.format("\nAMOUNT DUE: $%8.2f", getAmountDue());
+        return r;
+    }
+
+    private double getAmountDue()
+    {
+        double amountDue = 0;
+        for (LineItem item : items)
+        {
+            amountDue += item.getTotalPrice();
+        }
+        return amountDue;
+    }
 }
